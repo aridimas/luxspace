3143bc340fe99fb5005fa288fd58504a7111a873
aridimas <aridimas145@gmail.com>
2023-01-27 23:45:17 +0700

    +add (_) in the middel of sitesettings asu

--- database/migrations/2022_12_28_095108_create_site_settings_table.php
+++ database/migrations/2022_12_28_095108_create_site_settings_table.php
@@ -26,6 +26,7 @@ class CreateSiteSettingsTable extends Migration
             $table->string('twitter')->nullable();            
             $table->string('whatsapp')->nullable();            
             $table->string('email')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
 
@@ -41,6 +42,7 @@ class CreateSiteSettingsTable extends Migration
             'twitter' => 'www.twitter.com',
             'whatsapp' => 'wa.me/081248888888',
             'email' => 'admin@localbro',
+            'deleted_at' => date('Y-m-d H:i:s'),
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s')
 

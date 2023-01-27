018d62acedb54da010f0eab873ae8cfda032aa49
aridimas <aridimas145@gmail.com>
2023-01-27 23:11:28 +0700

    (-) soft deletes

--- database/migrations/2022_12_28_095108_create_site_settings_table.php
+++ database/migrations/2022_12_28_095108_create_site_settings_table.php
@@ -26,7 +26,7 @@ class CreateSiteSettingsTable extends Migration
             $table->string('twitter')->nullable();            
             $table->string('whatsapp')->nullable();            
             $table->string('email')->nullable();
-            $table->softDeletes();
+            
             $table->timestamps();
         });
 
@@ -42,7 +42,6 @@ class CreateSiteSettingsTable extends Migration
             'twitter' => 'www.twitter.com',
             'whatsapp' => 'wa.me/081248888888',
             'email' => 'admin@localbro',
-            'deleted_at' => date('Y-m-d H:i:s'),
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s')
 

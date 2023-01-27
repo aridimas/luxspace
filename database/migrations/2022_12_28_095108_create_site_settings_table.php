c311fe43a7221aa83a085a05266ef86df801d8bb
aridimas <aridimas145@gmail.com>
2023-01-27 23:02:52 +0700

    +nullable

--- database/migrations/2022_12_28_095108_create_site_settings_table.php
+++ database/migrations/2022_12_28_095108_create_site_settings_table.php
@@ -17,15 +17,15 @@ class CreateSiteSettingsTable extends Migration
         Schema::create('site_settings', function (Blueprint $table) {
             $table->id();
             $table->string('site_name');
-            $table->longText('site_description');
-            $table->string('logo_url');
-            $table->string('icon_url');
-            $table->string('thumbnail_url');
-            $table->string('facebook');
-            $table->string('instagram');            
-            $table->string('twitter');            
-            $table->string('whatsapp');            
-            $table->string('email');
+            $table->longText('site_description')->nullable();
+            $table->string('logo_url')->nullable();
+            $table->string('icon_url')->nullable();
+            $table->string('thumbnail_url')->nullable();
+            $table->string('facebook')->nullable();
+            $table->string('instagram')->nullable();            
+            $table->string('twitter')->nullable();            
+            $table->string('whatsapp')->nullable();            
+            $table->string('email')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });

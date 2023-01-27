dcac5210fcd01e49357935c03e6d21b6da536f9e
aridimas <aridimas145@gmail.com>
2023-01-27 22:37:47 +0700

    (-)deleted migration for add roles to users karna udh ada, masukin data default untuk website

--- database/migrations/2022_12_28_095108_create_site_settings_table.php
+++ database/migrations/2022_12_28_095108_create_site_settings_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateSiteSettingsTable extends Migration
 {
@@ -15,31 +16,36 @@ class CreateSiteSettingsTable extends Migration
     {
         Schema::create('site_settings', function (Blueprint $table) {
             $table->id();
-
             $table->string('site_name');
-
             $table->longText('site_description');
-
             $table->string('logo_url');
-
             $table->string('icon_url');
-
             $table->string('thumbnail_url');
-
             $table->string('facebook');
-
-            $table->string('instagram');
-            
-            $table->string('twitter');
-            
-            $table->string('whatsapp');
-            
+            $table->string('instagram');            
+            $table->string('twitter');            
+            $table->string('whatsapp');            
             $table->string('email');
-
             $table->softDeletes();
-
             $table->timestamps();
         });
+
+        DB::table('sitesetting')->insert([
+            'id' => 1,
+            'site_name' => 'Luxspace',
+            'site_description' => 'Merupakan tempat jual beli perabotan',
+            'logo_url' =>'storage/logo/logo.jpg',
+            'iconurl' =>'storage/icon/icon.jpg',
+            'thumbnail' => 'storage/thumbnail/thumbnail.jpg',
+            'facebook' => 'www.facebook.com',
+            'twitter' => 'www.twitter.com',
+            'whatsapp' => 'wa.me/081248888888',
+            'email' => 'admin@localbro',
+            'deleted_at' => null,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+
+        ]);
     }
 
     /**
--- resources/views/pages/dashboard/sitesetting/index.blade.php
+++ resources/views/pages/dashboard/sitesetting/index.blade.php
@@ -32,8 +32,7 @@
                         <div class="w-full px-3">
                             <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Nama Website</label>
                             <input type="text" value="@foreach ($sitesetting1 as $name){{$name->site_name}}"
-                            @endforeach
-                             name="site_name" class="block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                            @endforeach name="site_name" class="block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
                         </div>
                     </div>
                     <div class="flex flex-wrap -mx-3 mb-6">
